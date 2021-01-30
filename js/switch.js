63b030cf65488f6db0c030407f7d3c4004613f4f
Yuichi <ozwald176@gmail.com>
2021-01-30 13:08:34 +0900

    first commit

---
+++ js/switch.js
@@ -0,0 +1,19 @@
+"use strict"
+{
+    const nav_item = document.querySelectorAll(".nav_item");
+    const content_container = document.querySelectorAll(".content-container");
+
+    function change() {
+        // 最初に全てのコンテナからshowクラスをとる
+        content_container.forEach(item => {
+            item.classList.remove('show');
+        })
+
+        const show_content = document.getElementById(this.dataset.id);
+        show_content.classList.add('show');
+    }
+
+    nav_item.forEach(function(item){
+        item.addEventListener("click", change);
+    })
+}
